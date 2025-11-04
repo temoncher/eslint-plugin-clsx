c34e56a4c2b900b63cc7f4ff302ea2846ce08f5f
temoncher <temoncher@yandex.ru>
2025-11-04 11:29:45 +0300

    add declaration emit

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     entry: ['src/**/*.ts', '!src/**/*.test.ts', '!src/**/tests-setup.mjs'],
     bundle: false,
+    dts: true,
 });
