9f376bdbf7bb3196ad6edd50eb88a0a88e4c12b3
Artem Baranov <48395731+temoncher@users.noreply.github.com>
2025-11-04 12:04:44 +0300

    feat: declaration files and flat configs (#35)
    * add declaration emit
    
    * add flat configs, update readme

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { RuleModule } from '@typescript-eslint/utils/ts-eslint';
+import type { RuleModule, ClassicConfig, FlatConfig } from '@typescript-eslint/utils/ts-eslint';
 import * as R from 'remeda';
 
 import { PluginDocs } from './PluginDocs';
@@ -7,22 +7,41 @@ import { allRules, REPO_URL } from './rules/allRules.generated';
 const configFilters = {
     all: () => true,
     recommended: (rule: RuleModule<string, unknown[], PluginDocs>) => rule.meta?.docs?.recommended,
+} as const;
+
+// TODO: remove remeda, unnecessary dependency
+const rules = R.mapValues(allRules, (rule, ruleName) => ({
+    ...rule,
+    meta: {
+        ...rule.meta,
+        url: `${REPO_URL}/tree/HEAD/docs/rules/${ruleName}.md`,
+    },
+})) as {
+    [K in keyof typeof allRules]: (typeof allRules)[K] & {
+        meta: { url: `${typeof REPO_URL}/tree/HEAD/docs/rules/${K}.md` };
+    };
 };
 
+const configs = R.mapValues(configFilters, (configPredicate) => ({
+    plugins: ['clsx'],
+    rules: Object.fromEntries(
+        Object.entries(allRules)
+            .filter(([_ruleName, rule]) => configPredicate(rule))
+            .map(([ruleName]) => [`clsx/${ruleName}`, 1])
+    ),
+})) as Record<keyof typeof configFilters, ClassicConfig.Config>;
+
 export = {
-    rules: R.mapValues(allRules, (rule, ruleName) => ({
-        ...rule,
-        meta: {
-            ...rule.meta,
-            url: `${REPO_URL}/tree/HEAD/docs/rules/${ruleName}.md`,
-        },
-    })),
-    configs: R.mapValues(configFilters, (configPredicate) => ({
-        plugins: ['clsx'],
-        rules: Object.fromEntries(
-            Object.entries(allRules)
-                .filter(([_ruleName, rule]) => configPredicate(rule))
-                .map(([ruleName]) => [`clsx/${ruleName}`, 1])
-        ),
-    })),
+    rules,
+    configs: {
+        ...configs,
+        flat: R.mapValues(configFilters, (configPredicate) => ({
+            plugins: { clsx: { rules } },
+            rules: Object.fromEntries(
+                Object.entries(allRules)
+                    .filter(([_ruleName, rule]) => configPredicate(rule))
+                    .map(([ruleName]) => [`clsx/${ruleName}`, 1])
+            ),
+        })) as Record<keyof typeof configFilters, FlatConfig.Config>,
+    },
 };
